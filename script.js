01a23c61e1abd38135f6fee50a5cde4d7925f864
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-02-06 11:54:38 +0530

    add new api

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-async function getWeather() {
-    try {
-        const response = await fetch(
-            "https://api.open-meteo.com/v1/forecast?latitude=28.6139&longitude=77.2090&current_weather=true"
-        );
-        const data = await response.json();
-        console.log(data.current_weather);
-    } catch (error) {
-        console.error("Error fetching weather data:", error);
-    }
+const apikey = "299dfda9c8a24f0282855252250602";
+const apiUrl = "http://api.weatherapi.com/v1/current.json?key=299dfda9c8a24f0282855252250602&q=London&aqi=no";
+
+async function checkWeather(){
+    const response = await fetch(apiUrl + `&appid=${apikey}`);
+    var data = await response.json();
+
+    console.log(data);
 }
 
-getWeather();
+checkWeather();
