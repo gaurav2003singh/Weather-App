b5468624f5bc083084296a21fe0053715b45e5e1
Gaurav Singh <www.gschauhan2003@gmail.com>
2025-02-06 15:00:53 +0530

    final submission

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const apikey = "299dfda9c8a24f0282855252250602";
-const apiUrl = "http://api.weatherapi.com/v1/current.json?key=299dfda9c8a24f0282855252250602&q=London&aqi=no";
+const apiUrl =
+  "http://api.weatherapi.com/v1/current.json?key=299dfda9c8a24f0282855252250602&q= ";
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apikey}`);
-    var data = await response.json();
+  const searchBox = document.querySelector(".search input");
+  const searchBtn = document.querySelector(".search button");
 
-    console.log(data);
+
+ 
+
+
+
+
+
+async function checkWeather(city) { 
+  const response = await fetch(apiUrl + city + `&appid=${apikey}`);
+  var data = await response.json();
+
+  console.log(data);
+  document.querySelector(".city").innerHTML = data.location.name;
+  document.querySelector(".temp").innerHTML = Math.round(data.current.temp_c )+ "℃";
+  document.querySelector(".humidity").innerHTML = data.current.humidity + "%";
+  document.querySelector(".wind").innerHTML = data.current.wind_kph + " km/hr";
 }
 
-checkWeather();
+searchBtn.addEventListener("click", () =>{
+    checkWeather(searchBox.value);
+})
+
+
